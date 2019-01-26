f290d07940936b4cc35e2789e31b8a5610df94c1
ArjenCole-mpb <jinhesky@hotmail.com>
2019-01-26 15:17:23 +0800

    初步搭建静态网页

--- testDjangoApp/views.py
+++ testDjangoApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def index(request):
+    # request.POST
+    # request.Get
+    # return HttpResponse("hello world!")
+    if request.method == "POST":
+        username = request.POST.get("username",None)
+        password = request.POST.get("password",None)
+        print(username,password)
+    return render(request, "index.html",)
