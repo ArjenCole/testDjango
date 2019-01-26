f01dc3842bf30a0236f6ec80fafaf3ea42c0c8dc
ArjenCole-mpb <jinhesky@hotmail.com>
2019-01-26 23:28:24 +0800

    数据库插入数据

--- testDjangoApp/views.py
+++ testDjangoApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+import pymysql
+
 
 # Create your views here.
 user_list = [
@@ -8,13 +10,26 @@ user_list = [
 ]
 
 
+def get_conn():
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='test_schema')    # db:表示数据库名称
+    return conn
+
+
+def insert(sql, args):
+    conn = get_conn()
+    cur = conn.cursor()
+    result = cur.execute(sql, args)
+    print(result)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 def index(request):
-    # request.POST
-    # request.Get
-    # return HttpResponse("hello world!")
     if request.method == "POST":
-        username = request.POST.get("username", None)
-        password = request.POST.get("password", None)
-        tmp = {"user": username, "pwd": password}
-        user_list.append(tmp)
+        tusername = request.POST.get("username", None)
+        tpassword = request.POST.get("password", None)
+        sql = 'INSERT INTO test_table(NAME,PASSWORD)  VALUES(%s,%s);'
+        insert(sql, (tusername, tpassword))
+
     return render(request, "index.html", {"data": user_list})
