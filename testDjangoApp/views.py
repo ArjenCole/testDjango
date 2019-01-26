caec5ab7b774bdd13b8d46f38fbcc021101adb48
ArjenCole-mpb <jinhesky@hotmail.com>
2019-01-26 15:29:53 +0800

    网页数据提交及刷新

--- testDjangoApp/views.py
+++ testDjangoApp/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 # Create your views here.
+user_list = [
+    {"user": "jack", "pwd": "abc"},
+    {"user": "tom", "pwd": "ABC"},
+]
+
+
 def index(request):
     # request.POST
     # request.Get
     # return HttpResponse("hello world!")
     if request.method == "POST":
-        username = request.POST.get("username",None)
-        password = request.POST.get("password",None)
-        print(username,password)
-    return render(request, "index.html",)
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        tmp = {"user": username, "pwd": password}
+        user_list.append(tmp)
+    return render(request, "index.html", {"data": user_list})
